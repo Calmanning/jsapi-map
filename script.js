910a385e428ed656b2708e916be3366320add082
Calvin Manning <cal12071@esri.com>
2021-12-01 11:25:55 -0800

    simple map made. Also created css doc and linked to index HTML. Not sure how the zoom widgets came included -- will want to mess around with the mapview obj to see if that's what did it.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+console.log("just keep going.");
 console.log("testing js connection to index.");
+
+require(["esri/Map", "esri/views/MapView"], (Map, MapView) => {
+        const map = new Map({
+          basemap: "topo-vector"
+        });
+
+    const view = new MapView({
+        container: "viewDiv",
+        map: map,
+        zoom: 4,
+        center: [15, 65]
+    });
+});
+
+console.log("did we make it through?");
+
+
