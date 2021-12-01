2982325704d883e06ef0690fe01d282e75cf7af8
Calvin Manning <cal12071@esri.com>
2021-12-01 14:59:48 -0800

    alright. I got lost in some documentation...a LOT of documentation, but I was able to navigate through it and get (what I think)is a proper feature layer to deliniate major cites in the US. Side note: there are a LOT of layers. And I do not know how to take advantage of that yet!

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 console.log("just keep going.");
 console.log("testing js connection to index.");
 
-require(["esri/Map", "esri/views/MapView"], (Map, MapView) => {
-        const map = new Map({
-          basemap: "topo-vector"
+
+require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (esriConfig, Map, MapView, FeatureLayer) => {
+    
+    esriConfig.apiKey = "AAPK115d19ab66264ef1b7cdbdd54b6804f4whm-2t82h02UCQQ1zigAlbT-GPsbqzkH4Cd1xDjXtPoshgyibnsGBM4zg-eklxut";
+    
+    const map = new Map({
+          basemap: "streets-vector"
         });
 
-    const view = new MapView({
-        container: "viewDiv",
-        map: map,
-        zoom: 4,
-        center: [15, 65]
-    });
+        const fl = new FeatureLayer({
+            url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/Enriched%20USA%20Major%20Cities/FeatureServer"
+        });
+
+
+
+        map.add(fl, 0);
+        // not sure if I need to add the below for anything...I got lost in a see of documentation.
+        // fl.load().then();
+
+        const view = new MapView({
+            container: "viewDiv",
+            map: map,
+            zoom: 4,
+            center: [253, 42]
+        });
 });
 
 console.log("did we make it through?");
