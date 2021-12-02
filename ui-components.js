add0e5d916f14fd926086c0bb010f62500211c2a
Calvin Manning <cal12071@esri.com>
2021-12-02 14:34:08 -0800

    reverting file structre. Welp! Turns out my separate file-structe system didn't quite work...but I can't quiet figure out why. I'm wondering if it's a compile-oriented issue.

---
+++ ui-components.js
@@ -0,0 +1,3 @@
+console.log("here's to trying something new. UI connected");
+
+// separated out the UI from the mapView file. It seems to link up just fine...but I can't tell if I'm doing something wrong. FOLLOW-UP - I shouldn't work. it should 
